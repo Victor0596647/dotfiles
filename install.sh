912927a8f13c740fec367f51b3268a7b1c8e7c6f
Victor0596647 <mrvictor059@gmail.com>
2024-11-27 15:49:18 +0200

    Coded shell script to automatically copy files

---
+++ install.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+listConfigs(){
+    configs=()
+    for dir in $(ls $(dirname $0)); do
+        if [ -d ${dir} ]; then
+            configs+=("${dir}")
+        fi
+    done
+    echo "${configs[@]}"
+}
+
+copyConfigs(){
+    configs=("${*}")
+    for config in "${configs[@]}"; do
+        echo "copying $config to $HOME/.config/"
+        if [ ! -e "$HOME/.config/${config}" ]; then
+            mkdir "$HOME/.config/${config}"
+            cp -r "${config}" "$HOME/.config/"
+        else
+            echo "${config} directory exists! Remove it to install correctly. Skipping..."
+        fi
+    done
+}
+
+configsToInstall=${*}
+if [ -z "${configsToInstall[*]}" ]; then
+    echo "You can install these available configs:" $(listConfigs)
+    echo "Simply run './install.sh [config name]'"
+    echo "Or run './install.sh all' to install all at once"
+else
+    if [ "$1" == "all" ]; then
+        copyConfigs "$(listConfigs)"
+    else
+        copyConfigs "${configsToInstall[*]}"
+    fi
+fi
