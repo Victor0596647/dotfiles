2baf1cdfee6509c4040b3372e32b70dc4c72ec84
Victor0596647 <mrvictor059@gmail.com>
2024-12-12 12:53:10 +0200

    changed some code to specifically install .zshrc file

--- install.sh
+++ install.sh
@@ -13,12 +13,21 @@ listConfigs(){
 copyConfigs(){
     configs=(${@})
     for config in ${configs[@]}; do
-        echo "copying $config to $HOME/.config/"
-        if [ ! -e "$HOME/.config/${config}" ]; then
-            mkdir "$HOME/.config/${config}"
-            cp -r "${config}" "$HOME/.config/"
-        else
-            echo "${config} directory exists! Remove it to install correctly. Skipping..."
+        if [ ${config} == "zsh" ]; then 
+            echo "copying $config to $HOME/"
+            if [ ! -e "$HOME/.zshrc" ]; then
+                cp "${config}/.zshrc" "$HOME/"
+            else
+                echo ".zshrc file already exists! Remove it and try again. Skipping..."
+            fi
+        else 
+            echo "copying $config to $HOME/.config/"
+            if [ ! -e "$HOME/.config/${config}" ]; then
+                mkdir "$HOME/.config/${config}"
+                cp -r "${config}" "$HOME/.config/"
+            else
+                echo "${config} directory exists! Remove it to install correctly. Skipping..."
+            fi
         fi
     done
     echo "Done copying..."
